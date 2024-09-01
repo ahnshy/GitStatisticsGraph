adeb1950df98a6addff093289c8d6be38b33acb2
ahnshy <ahnshy@naver.com>
2024-09-01 21:32:45 +0900

    <feat> : Add get git revision history function <feat> : Add get git url form vue

---
+++ src/components/InputRepository.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "InputRepository"
+}
+</script>
+
+<template>
+  <p>Repository: </p>
+  <input v-model="gitRepository" placeholder="Input Repository URL. git://" />
+</template>
+
+<style scoped>
+
+</style>
